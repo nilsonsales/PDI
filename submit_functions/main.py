f195e4f6ec3caa3ec600b59282a4221d9f5c7e64
Nilson Sales <nilsonsaless@gmail.com>
2021-03-19 21:23:49 -0300

    Changing union to grayscale

--- submit_functions/main.py
+++ submit_functions/main.py
@@ -42,11 +42,14 @@ def logical_operations(img1, img2):
 # %%
 
 def union(img1, img2):
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
     result = cv2.max(img1, img2)
 
-    plt.subplot(131); plt.title("IMG 1"); plt.imshow(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB))
-    plt.subplot(132); plt.title("IMG 2"); plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
-    plt.subplot(133); plt.title("Union"); plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+    plt.subplot(131); plt.title("IMG 1"); plt.imshow(img1, cmap='gray')
+    plt.subplot(132); plt.title("IMG 2"); plt.imshow(img2, cmap='gray')
+    plt.subplot(133); plt.title("Union"); plt.imshow(result, cmap='gray')
     plt.show()
 
 #%%
